f14dad3281050ec9dbf50ed17db30ec103dab683
claretsword <blackurohei@gmail.com>
2024-03-26 23:50:03 +0900

    으어

--- Lecture03-HW/Lecture03-HW.cpp
+++ Lecture03-HW/Lecture03-HW.cpp
@@ -15,25 +15,49 @@ enum ForeColour {
 int main()
 {
     std::cout << "\x1b[033m" << "아름다운 색!! 우왕";
-    /*
-    * 흰색 블록을 그리는 키코드 : 1 (1)
-    * 빨강색 블록을 그리는 키코드 : 2 (10)
-    * 녹색 블록을 그리는 키코드 : 4 (100)
-    * 노란색 블록을 그리는 키코드 : 8 (1000)
-    * 청록색 블록을 그리는 키코드 : 16 (10000)
-    * 마젠타색 블록을 그리는 키코드 : 32 (100000)
-    * 프로그램 종료 : 64 (1000000)
-    *
-    * 프로그램을 시작하면 아래와 같이 쓰시오
-    *
-    *
-    * 화면에 그림을 그리는 프로그램입니다.
-    * 학번 : 349218347(본인학번)
-    * 이름 : 본인이름
-    *
-    * 화면에 그릴 물체코드를 입력하세요 :
-    * (반복)
-    */
 
+    std::cout << std::endl << std::endl;
+    std::cout << "화면에 그림을 그리는 프로그램입니다." << std::endl;
+    std::cout << "학번 : 202027037" << std::endl; 
+    std::cout << "이름 : 조경호" << std::endl; 
+    std::cout << std::endl;
 
+    int objectCode;
+
+    std::cout << "화면에 그릴 물체코드를 입력하세요 : ";
+
+    while (true) {
+        std::cin >> objectCode;
+
+        switch (objectCode) {
+        case 1:
+            std::cout << "\x1b[" << White << "m■"; 
+            break;
+        case 2:
+            std::cout << "\x1b[" << Red << "m■";
+            break;
+        case 4:
+            std::cout << "\x1b[" << Green << "m■";
+            break;
+        case 8:
+            std::cout << "\x1b[" << Yellow << "m■";
+            break;
+        case 16:
+            std::cout << "\x1b[" << Cyan << "m■";
+            break;
+        case 32:
+            std::cout << "\x1b[" << Magenta << "m■";
+            break;
+        case 64:
+            std::cout << "프로그램을 종료합니다." << std::endl;
+            return 0;
+        default:
+            std::cout << "잘못된 물체코드입니다." << std::endl;
+        }
+
+        std::cout << std::endl;
+        std::cout << "화면에 그릴 물체코드를 입력하세요 : ";
+    }
+
+    return 0;
 }
